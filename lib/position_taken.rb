e7db2397679ab305275076ffbdeb8610345bd103
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-16 18:54:52 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
-def position_taken? 
+def position_taken?
   if board[index]==" "||board[index]==""|| board[index]== nil
-    return false 
-  else 
-    return true 
+    return false
+  else
+    return true
   end 
